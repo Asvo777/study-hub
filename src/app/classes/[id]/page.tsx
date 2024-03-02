131e05f9ff7661407d4fadd91b1a1722170e370b
Benjamin Sam <79420331+bvsam@users.noreply.github.com>
2024-03-02 02:22:14 -0500

    Adding basic nav

---
+++ src/app/classes/[id]/page.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+export default function Page({ params }: { params: { id: string } }) {
+  const title = params.id.toUpperCase();
+
+  return (
+    <div className="flex flex-col">
+      <div className="text-2xl bg-red-200 p-5">
+        <span className="font-bold">{title}</span>
+      </div>
+      <div className="flex flex-row gap-10 bg-blue-200 p-5">
+        <Link href={`${params.id}/q-and-a`}>
+          <div>Q&A</div>
+        </Link>
+        <Link href={`${params.id}/project`}>
+          <div>Project</div>
+        </Link>
+        <Link href={`${params.id}/chatbot`}>
+          <div>Chatbot</div>
+        </Link>
+      </div>
+    </div>
+  );
+}
--- src/app/classes/[id]/q-and-a/page.tsx
+++ src/app/classes/[id]/q-and-a/page.tsx
@@ -1,3 +1,3 @@
 export default function Page() {
-  return <>Hello! 123</>;
+  return <>Q&A</>;
 }
