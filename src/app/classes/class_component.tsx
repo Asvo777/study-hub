e8c4ed170ff92d362fefa33a398124328614ca8b
Asvo777 <126938639+Asvo777@users.noreply.github.com>
2024-03-02 10:39:52 -0500

    CHanges

--- src/app/classes/class_component.tsx
+++ src/app/classes/class_component.tsx
@@ -1,13 +1,19 @@
+import Link from "next/link";
+
 type ClassProps = {
   title: string;
+  id: string
 };
 
 export default function Class(props: ClassProps) {
   return (
     <>
-      <div className="border border-slate-600 p-5">
-        <h2>{props.title}</h2>
-      </div>
+        <Link href={`/classes/${props.id}`}>
+            <div className="border border-slate-600 p-5 min-h-40 bg-slate-100 rounded-md text-xl">
+                <h2 className="font-bold">{props.title}</h2>
+            </div>
+        </Link>
+        
     </>
   );
 }
--- src/app/classes/page.tsx
+++ src/app/classes/page.tsx
@@ -1,13 +1,21 @@
 import Class from "./class_component";
 
+
 export default function Page() {
+  const carousel = false
+  // const [carousel, setCarousel] = useState(true);
+
+  
   return (
-    <div className="grid grid-cols-4 gap-4 p-5">
-      <Class title="Calculus 101" />
-      <Class title="English 101" />
-      <Class title="Science 101" />
-      <Class title="History 101" />
-      <Class title="History 555" />
-    </div>
+    carousel? 
+    <></> // Put the courses in a carousel format
+     :
+    (<div className="grid grid-cols-1 gap-4 p-5">
+      <Class id="mat-101" title="Calculus 101" />
+      <Class id="mat-101" title="English 101" />
+      <Class id="mat-101" title="Science 101" />
+      <Class id="mat-101" title="History 101" />
+      <Class id="mat-101" title="History 555" />
+    </div>)
   );
 }
