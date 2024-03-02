7e50cd0d62ee36294403cf669b912c669b42b3fb
Benjamin Sam <79420331+bvsam@users.noreply.github.com>
2024-03-02 01:35:09 -0500

    Basic class components

--- src/app/classes/class_component.tsx
+++ src/app/classes/class_component.tsx
@@ -1,14 +1,13 @@
 type ClassProps = {
-    title:string
-}
+  title: string;
+};
 
-export default function Class(props:ClassProps) {
-    return <>
-      <div >
+export default function Class(props: ClassProps) {
+  return (
+    <>
+      <div className="border border-slate-600 p-5">
         <h2>{props.title}</h2>
       </div>
-    </>;
-  }
-
-
-
+    </>
+  );
+}
--- src/app/classes/page.tsx
+++ src/app/classes/page.tsx
@@ -1,18 +1,13 @@
 import Class from "./class_component";
 
 export default function Page() {
-  return <>
-    <Class
-      title="Calculus 101"
-    />
-    <Class
-      title="English 101"
-    />
-    <Class
-      title="Science 101"
-    />
-    <Class
-      title="History 101"
-    />
-  </>;
+  return (
+    <div className="grid grid-cols-4 gap-4 p-5">
+      <Class title="Calculus 101" />
+      <Class title="English 101" />
+      <Class title="Science 101" />
+      <Class title="History 101" />
+      <Class title="History 555" />
+    </div>
+  );
 }
