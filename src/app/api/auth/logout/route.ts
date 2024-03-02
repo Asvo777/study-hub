0f5bdac68aef32f0ddbaae13511c712c331f7f88
Benjamin Sam <79420331+bvsam@users.noreply.github.com>
2024-03-02 17:46:36 -0500

    Minor fix to logout api

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -1,7 +1,7 @@
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
-export async function GET(request: Request) {
+export async function GET() {
   cookies().delete("currentUser");
 
   return NextResponse.json({ status: 200 });
